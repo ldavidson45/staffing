363e745992666ecb24a1df65024e24f6d5426af4
Liz Davidson <lizdavidson45@gmail.com>
2019-04-08 19:05:35 -0400

    create custom users app and models

--- staffing_django/settings.py
+++ staffing_django/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'staffing'
+    'staffing',
+    'users'
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
