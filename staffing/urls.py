51fb293a73db3cf665c01194f2d56cd2b5f1af14
Liz Davidson <lizdavidson45@gmail.com>
2019-04-09 10:04:08 -0400

    create logout template and url

--- staffing/urls.py
+++ staffing/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('profile/<int:pk>/company', views.create_company, name="company_create"),
     path('company/<int:pk>', views.company_detail, name='company_detail'),
     path('', views.employee_list, name="employee_list"),
-    path('login/', auth_views.LoginView.as_view(template_name='login_page.html'))
-
+    path('login/', auth_views.LoginView.as_view(template_name='login_page.html')),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'))
 ]
