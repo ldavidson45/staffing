d3dde5d747be64cbc22733d5af4ea4c8303eb962
Liz Davidson <lizdavidson45@gmail.com>
2019-04-08 21:32:51 -0400

    edit signup to redirect to the company form

--- staffing/urls.py
+++ staffing/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.sign_up, name='signup'),
+    path('profile/<int:pk>/company', views.create_company, name="company_create")
 ]
--- staffing/views.py
+++ staffing/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
-from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CompanyForm
+from django.contrib.auth import login, authenticate
 from django.urls import reverse_lazy
 from django.views import generic
+from .models import Company, Profile
 # Create your views here.
 
-class SignUp(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('signup')
-    template_name = 'signup.html'
+# class SignUp(generic.CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('signup')
+#     template_name = 'signup.html'
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            profile= Profile.objects.create(user = user)
+            login(request, user)
+            return redirect('company_create', pk=profile.pk)
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def create_company(request, pk):
+    if request.method == 'POST':
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+            company = form.save()
+            Profile.objects.filter(pk=pk).update(company=company)
+            return redirect('/')
+    else:
+        form = CompanyForm()
+    return render(request, 'signup.html', {'form': form})
