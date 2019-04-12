071e67776fb02b98dc80d55e0fa2e890c8e8511a
Liz Davidson <lizdavidson45@gmail.com>
2019-04-12 11:54:09 -0400

    change settings

--- staffing_django/settings.py
+++ staffing_django/settings.py
@@ -138,16 +138,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # django-heroku.settings(locals())
