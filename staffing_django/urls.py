a1fb17f5ca9430d84b2ebceb0086839ec3619a05
Liz Davidson <lizdavidson45@gmail.com>
2019-04-12 10:15:43 -0400

    add static root file path

--- staffing_django/urls.py
+++ staffing_django/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('staffing.urls')),
     path('', include('django.contrib.auth.urls')),
-    path('', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('rest_framework.urls', namespace='rest_framework')),
+
 
 ]
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
